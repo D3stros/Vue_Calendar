87a348ce9226f24b952424a045bcc95bbb5107a7
D3stros <patrick.fallmann@hotmail.com>
2019-10-27 14:06:16 +0100

    Added functionality to edit and close event details

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -55,28 +55,36 @@
           v-model="selectedOpen"
           :close-on-content-click="false"
           :activator="selectedElement"
-          full-width
           offset-x
         >
           <v-card color="grey lighten-4" min-width="350px" flat>
             <v-toolbar :color="selectedEvent.color" dark>
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
+              <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+                <v-icon>mdi-delete</v-icon>
               </v-btn>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
             </v-toolbar>
             <v-card-text>
-              <span v-html="selectedEvent.details"></span>
+              <form v-if="currentlyEditing !== selectedEvent.id">{{selectedEvent.details}}</form>
+              <form v-else>
+                <textarea-autosize
+                  v-model="selectedEvent.details"
+                  type="text"
+                  style="width: 100%"
+                  :min-height="100"
+                  placeholder="add note"
+                ></textarea-autosize>
+              </form>
             </v-card-text>
             <v-card-actions>
-              <v-btn text color="secondary" @click="selectedOpen = false">Cancel</v-btn>
+              <v-btn text color="secondary" @click="selectedOpen = false">Close</v-btn>
+              <v-btn
+                text
+                v-if="currentlyEditing !== selectedEvent.id"
+                @click.prevent="editEvent(selectedEvent)"
+              >Edit</v-btn>
+              <v-btn text v-else @click.prevent="updateEvent(selectedEvent)">Save</v-btn>
             </v-card-actions>
           </v-card>
         </v-menu>
@@ -160,6 +168,16 @@ export default {
       })
       this.events = events
     },
+    async updateEvent(ev) {
+      await db
+        .collection('calEvent')
+        .doc(this.currentlyEditing)
+        .update({
+          details: ev.details
+        })
+      this.selectedOpen = false
+      this.currentlyEditing = null
+    },
     viewDay({ date }) {
       this.focus = date
       this.type = 'day'
@@ -175,6 +193,9 @@ export default {
     },
     next() {
       this.$refs.calendar.next()
+    },
+    editEvent(ev) {
+      this.currentlyEditing = ev.id
     },
     showEvent({ nativeEvent, event }) {
       const open = () => {
